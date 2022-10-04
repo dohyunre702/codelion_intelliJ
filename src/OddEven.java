bea81c39c3aee5c8b12b7236d5241ffa074e2b22
dohyu <dohyunre702@gmail.com>
2022-10-04 15:13:17 +0900

    codeup1156.

--- src/OddEven.java
+++ src/OddEven.java
@@ -4,7 +4,6 @@ public class OddEven {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        System.out.print("숫자를 입력해주세요 >>");
         int num = sc.nextInt();
 
         if (num % 2 == 0) {
