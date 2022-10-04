2c25cf2e9f104e58ab3dab6662fe96b52c3cad5b
dohyu <dohyunre702@gmail.com>
2022-10-04 13:21:48 +0900

    Array10개

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         //alt shift f10;
-        System.out.println("Hello world!");
+        int [] array = new int[10];
 
+        for(int i =0; i < array.length; i++ ) {
+            array[i] = i;
+        }
+
+        for(int i =0; i < array.length; i++) {
+            System.out.print(array[i] + ",");
+        }
     }
 }
