a174c33d9ad0f100efc9dce761f086915165b248
dohyu <dohyunre702@gmail.com>
2022-10-05 15:33:28 +0900

    221005_//list_add예제

---
+++ src/week3/collection/LikeLion2th.java
@@ -0,0 +1,18 @@
+package week3.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LikeLion2th {
+    private List<String> student = new ArrayList<>();
+
+    public LikeLion2th(){
+        Names names = new Names();
+        this.student = names.names();
+
+    }
+    //멋사자 2기 학생의 이름이 들어있는 list를 return하는 method
+    public List<String> getStudentList() {
+        return this.student;
+    }
+}
