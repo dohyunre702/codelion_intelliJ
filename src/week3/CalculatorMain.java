0432b36719bab68e8a8cd16525b6fee27006f416
dohyu <dohyunre702@gmail.com>
2022-10-05 10:32:04 +0900

    221005

---
+++ src/week3/CalculatorMain.java
@@ -0,0 +1,13 @@
+package week3;
+
+public class CalculatorMain {
+    public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+
+        System.out.println(Calculator.plus(a,b));
+        System.out.println(Calculator.minus(a,b));
+        System.out.println(Calculator.multiple(a,b));
+        System.out.println(Calculator.divide(a,b));
+    }
+}
