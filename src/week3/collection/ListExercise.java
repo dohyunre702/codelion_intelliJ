a32863aa4f1ef10c41707b7071fd02e8dedbe21b
dohyu <dohyunre702@gmail.com>
2022-10-06 09:29:22 +0900

    221006//리스트 프레임웍 복습

---
+++ src/week3/collection/ListExercise.java
@@ -0,0 +1,20 @@
+package week3.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ListExercise {
+    List<String> students;
+
+    public ListExercise(){
+        this.students = new ArrayList<>();
+        this.students.add("나");
+        this.students.add("김미지");
+        this.students.add("김미경");
+        this.students.add("김경록");
+    }
+    public List<String> getStudents(){
+        return this.students;
+    }
+}
