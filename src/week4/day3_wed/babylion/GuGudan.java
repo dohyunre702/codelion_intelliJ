c9657280a0ca90eb5d7feb8727a72f8d8d2d70c3
dohyu <dohyunre702@gmail.com>
2022-10-12 17:45:32 +0900

    221012_아기사자반실습

--- src/week4/day3_wed/babylion/GuGudan.java
+++ src/week4/day3_wed/babylion/GuGudan.java
@@ -2,14 +2,27 @@ package week4.day3_wed.babylion;
 
 import java.util.Scanner;
 
+//메소드, 파라미터 적용 > 다형성 적용 필요
 public class GuGudan {
+
+    public void makegugudan (int n) {
+        for (int i = 1; i < 10; i++) {
+            System.out.println(n + "x" + i + "=" + n * i);
+        }
+    }
+
+    public void makeplusdan (int n) {
+        for (int i = 1; i < 10; i++) {
+            System.out.println(n + "+" + i + "=" + n+1);
+        }
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
 
-        for(int i = 1; i < 10; i++) {
-            int result = num * i;
-            System.out.println(num + "x" + i + "=" + num * i);
-        }
+        GuGudan gugudan = new GuGudan();
+        gugudan.makegugudan(num);
+        gugudan.makeplusdan(num);
+     }
     }
-}
