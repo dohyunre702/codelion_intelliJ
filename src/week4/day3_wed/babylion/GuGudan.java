151aba2850f396a009df391e7adcb25b6a2bc3ed
dohyu <dohyunre702@gmail.com>
2022-10-12 17:32:13 +0900

    221012_아기사자반실습

--- src/week4/day3_wed/babylion/GuGudan.java
+++ src/week4/day3_wed/babylion/GuGudan.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 public class GuGudan {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
 
         for(int i = 1; i < 10; i++) {
-            int num = sc.nextInt();
             int result = num * i;
             System.out.println(num + "x" + i + "=" + num * i);
         }
