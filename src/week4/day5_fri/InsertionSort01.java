0a8a1d73187de24aeb330f0179794a008c2ae327
dohyu <dohyunre702@gmail.com>
2022-10-14 10:22:33 +0900

    재귀 용법

--- src/week4/day5_fri/InsertionSort01.java
+++ src/week4/day5_fri/InsertionSort01.java
@@ -1,22 +1,29 @@
 package week4.day5_fri;
 
 import java.util.Arrays;
-//
+
 public class InsertionSort01 {
     public int[] sort(int[] arr, int i) {
         //1. i = 1일 때 arr[1] j vs arr[0]
         //2-1. i = 2일 때 arr[2] j vs arr[1]
         //2-2. i = 2일 때 arr[1] j vs arr[0]?
-        int j = i;
-        if (arr[j] < arr[j-1]) {
-            //swap
-            int tmp = arr[j-1];
-            arr[j-1] = arr[j];
-            arr[j] = tmp;
-        }
-        return arr; //[5, 8, 6, 2, 4]
+        //j는 루프를 돌면서 바꾸어야 함
 
+        //재귀 = 자기자신 호출. 반복문(while) 없이 반복하려 할 때
+        if (i==arr.length)return arr;
+
+        for (int j = i; j > 0; j--) {
+            System.out.printf("i:%d j:%d j-1:%d\n", i, j, j-1);
+            if (arr[j] < arr[j-1]) {
+                //swap
+                int tmp = arr[j-1];
+                arr[j - 1] = arr[j];
+                arr[j] = tmp;
+            }
+        }
+        return sort(arr, i+1); //[5, 8, 6, 2, 4]
     }
+
 
     public static void main(String[] args) {
         int[] arr = {8,5,6,2,4};
