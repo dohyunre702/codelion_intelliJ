e3254ad926a3d60cd6a4c215432e6bb418a2e4d3
dohyu <dohyunre702@gmail.com>
2022-10-14 10:28:23 +0900

    최종:루프 i 적용

--- src/week4/day5_fri/InsertionSort01.java
+++ src/week4/day5_fri/InsertionSort01.java
@@ -3,25 +3,19 @@ package week4.day5_fri;
 import java.util.Arrays;
 
 public class InsertionSort01 {
-    public int[] sort(int[] arr, int i) {
-        //1. i = 1일 때 arr[1] j vs arr[0]
-        //2-1. i = 2일 때 arr[2] j vs arr[1]
-        //2-2. i = 2일 때 arr[1] j vs arr[0]?
-        //j는 루프를 돌면서 바꾸어야 함
-
-        //재귀 = 자기자신 호출. 반복문(while) 없이 반복하려 할 때
-        if (i==arr.length)return arr;
-
-        for (int j = i; j > 0; j--) {
-            System.out.printf("i:%d j:%d j-1:%d\n", i, j, j-1);
-            if (arr[j] < arr[j-1]) {
-                //swap
-                int tmp = arr[j-1];
-                arr[j - 1] = arr[j];
-                arr[j] = tmp;
+    public int[] sort(int[] arr) {
+        //루프 i 적용
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = i; j > 0; j--) {
+//              System.out.printf("i:%d j:%d j-1:%d\n", i, j, j-1);
+                if (arr[j] < arr[j-1]) {
+                    int tmp = arr[j-1];
+                    arr[j-1] = arr[j];
+                    arr[j] = tmp;
+                }
             }
         }
-        return sort(arr, i+1); //[5, 8, 6, 2, 4]
+        return arr; //[5, 8, 6, 2, 4]
     }
 
 
@@ -30,7 +24,7 @@ public class InsertionSort01 {
 
         InsertionSort01 is = new InsertionSort01();
         arr = new int[]{5,8,6,2,4};
-        int[] result = is.sort(arr, 2);
+        int[] result = is.sort(arr);
         System.out.println(Arrays.toString(result));
     }
 
