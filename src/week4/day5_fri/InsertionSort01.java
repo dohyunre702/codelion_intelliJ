01559c92f38b55f6b84c29fc30b70b7cd695a2f9
dohyu <dohyunre702@gmail.com>
2022-10-14 09:41:24 +0900

    221014_일일알고리즘 기본 템플릿

---
+++ src/week4/day5_fri/InsertionSort01.java
@@ -0,0 +1,21 @@
+package week4.day5_fri;
+
+import java.util.Arrays;
+
+public class InsertionSort01 {
+    public int[] sort(int[] arr) {
+        int i = 1;
+
+
+        return new int[]{};
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {8,5,6,2,4};
+
+        InsertionSort01 is = new InsertionSort01();
+        int[] result = is.sort(arr);
+        System.out.println(Arrays.toString(result));
+    }
+
+}
