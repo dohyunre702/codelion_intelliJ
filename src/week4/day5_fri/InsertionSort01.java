9de6ba3c6a09e593c062defb921165432129d4ac
dohyu <dohyunre702@gmail.com>
2022-10-14 10:12:29 +0900

    j변수 추가

--- src/week4/day5_fri/InsertionSort01.java
+++ src/week4/day5_fri/InsertionSort01.java
@@ -3,14 +3,16 @@ package week4.day5_fri;
 import java.util.Arrays;
 
 public class InsertionSort01 {
-    public int[] sort(int[] arr) {
-        int i = 1;
-        int tmp = 0;
-        if (arr[i] < arr[i-1]) {//arr[1] vs arr[0]
+    public int[] sort(int[] arr, int i) {
+        //1. i = 1일 때 arr[1] j vs arr[0]
+        //2-1. i = 2일 때 arr[2] j vs arr[1]
+        //2-2. i = 2일 때 arr[1] j vs arr[0]?
+        int j = i;
+        if (arr[j] < arr[j-1]) {
             //swap
-            tmp = arr[i];
-            arr[i] = arr[i-1];
-            arr[i-1] = tmp;
+            int tmp = arr[j-1];
+            arr[j-1] = arr[j];
+            arr[j] = tmp;
         }
         return arr; //[5, 8, 6, 2, 4]
 
@@ -20,7 +22,8 @@ public class InsertionSort01 {
         int[] arr = {8,5,6,2,4};
 
         InsertionSort01 is = new InsertionSort01();
-        int[] result = is.sort(arr);
+        arr = new int[]{5,8,6,2,4};
+        int[] result = is.sort(arr, 2);
         System.out.println(Arrays.toString(result));
     }
 
