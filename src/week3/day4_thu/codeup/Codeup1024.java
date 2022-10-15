ce85e5fc67f85a437c11feeab8a14f9f0b178fa8
dohyu <dohyunre702@gmail.com>
2022-10-15 13:25:58 +0900

    3번째 시도 - char과 string 형변환 > str을 char타입으로 통일

--- src/week3/day4_thu/codeup/Codeup1024.java
+++ src/week3/day4_thu/codeup/Codeup1024.java
@@ -4,17 +4,18 @@ import java.util.Scanner;
 
 // 시도2. charAt(int index) 사용.
 // 문제 : 컴파일 에러. String valueOf로 charAt을 감싸주어야 함
-// 해결1. : charAt(i)를 string 타입으로 변경
+// 원인 : str과 char 사이의 포맷이 맞지 않아서 발생
+// 해결2. : str을 char 타입으로 변경
 
 public class Codeup1024 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
 
-        String str = "";
+        char ch;
         for (int i = 0; i < input.length(); i++) {
-            str = String.valueOf(input.charAt(i));
-            System.out.println(str);
+            ch = input.charAt(i);
+            System.out.println(ch);
         }
     }
 }
