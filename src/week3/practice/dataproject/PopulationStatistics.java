05cc855e3235e57b37ba30cf8e1d1006d934abe9
dohyu <dohyunre702@gmail.com>
2022-10-17 22:49:00 +0900

    Java로 대용량데이터 분석하기 복습 1. readByLine 리스트로 객체화, map으로 저장 2. parsing 안되어서 멈춰있는 상태

--- src/week3/practice/dataproject/PopulationStatistics.java
+++ src/week3/practice/dataproject/PopulationStatistics.java
@@ -3,33 +3,59 @@ import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class PopulationStatistics {
+    //1. 파일로부터 문자열 읽어오기. 리스트로 객체화
+    public List<PopulationMove> readByLine(String filename) throws IOException {
 
+        List<PopulationMove> pml = new ArrayList<>();
+        BufferedReader br = new BufferedReader(new FileReader(filename)); //파일 읽어올 준비
+
+        String str;
+        while ((str = br.readLine()) != null) {
+                PopulationMove pm = parse(str);
+                pml.add(pm);//리스트에 넣어준다
+            }
+            br.close();
+            return pml; //리스트 반환
+        }
+
+    //2. 읽어 온 문자열 파싱
     public PopulationMove parse(String data) {
-        int fromSido = 0, toSido = 0;
         //생성자 오버로딩으로 바로 매개변수를 받는 게 가능해짐
         //split()메소드로 쪼개어진 문자열을 > int로 변환
-        String[] split = data.split(",");
-        return new PopulationMove(split[0], split[6]);
+        try { //parsing이 안되어서 (numberformatexception) 일시정지 상태
+            String[] split = data.split(",");
+            Integer fromSido = Integer.valueOf(split[0]);
+            Integer toSido = Integer.valueOf(split[6]);
+            return new PopulationMove(fromSido, toSido);
+        } catch (NumberFormatException e) {
+        };
     }
 
-    public void readByLine(String filename) {//에러를 try-catch로 처리
-        String line;
-        try (BufferedReader br = Files.newBufferedReader(Paths.get(filename), StandardCharsets.UTF_8)) {
-            while ((line = br.readLine()) != null) {
-                System.out.println(line);
+    //3. List로 객체화해서 이사 간 횟수 구하기
+    public Map<String, Integer> getMoveCntMap(List<PopulationMove> pml) { //입력 타입 : list, 반환 타입: map
+        Map<String, Integer> moveCntMap = new HashMap<>();
+        for (PopulationMove pm : pml) {
+            String key = pm.getFromSido() + "," + pm.getToSido();
+            if (moveCntMap.get(key) == null) {
+                moveCntMap.put(key,1);
             }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+            moveCntMap.put(key, moveCntMap.get(key) + 1);
         }
+        return moveCntMap;
     }
 
     //메인
     public static void main(String[] args) throws IOException {
         PopulationStatistics ps = new PopulationStatistics();
-        String data = "41,650,25000,2021,05,21,11,290,68500,3,8,059,2,,,,,,,,,,,,,,,,,,,,,,,,,,,,347226";
-        PopulationMove populationMove = ps.parse(data);
+        //data를 주소로 변경
+        String address = "C:/Users/dohyu/git/codelion_intelliJ/src/week3/practice/dataproject/2021populationstat";
+        PopulationMove populationMove = ps.parse(address);
         System.out.println(populationMove.getFromSido());
         System.out.println(populationMove.getToSido());
     }
