393c6746a4022322c64c29b2c2660ce7f08df835
dohyu <dohyunre702@gmail.com>
2022-10-17 22:00:44 +0900

    Java로 대용량데이터 분석하기 복습 1. 파일 읽어오는 3가지 방법 2. PopulationMove getter, setter 3. parser 구현

--- src/week3/practice/dataproject/PopulationMove.java
+++ src/week3/practice/dataproject/PopulationMove.java
@@ -5,16 +5,7 @@ public class PopulationMove {
     private int fromSido; //전입
     private int toSido; //전출
 
-    //constructor 추가 fromSido, toSido를 받아 멤버변수에 넣는 기능
-    public PopulationMove(int fromSido, int toSido) {
-        this.fromSido = fromSido;
-        this.toSido = toSido;
-    }
-    //오버로딩
-    public PopulationMove(String fromSido, String toSido){
-        this.fromSido = Integer.parseInt(fromSido);
-        this.toSido = Integer.parseInt(toSido);
-    }
+
 
     //getter
     public int getFromSido() {
